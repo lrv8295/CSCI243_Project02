1d7079266ca623fb04e41ad45ae19e6083da6347
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-27 23:55:50 -0500

    set up

---
+++ tree_node.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tree_node.h"
+
+tree_node_t *make_interior(op_type_t op, char *token, tree_node_t *left, tree_node_t *right);
+
+tree_node_t *make_leaf(exp_type_t exp_type, char *token) {}
+
