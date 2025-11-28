4c8ea952357dee2627a3e1dc959d37bbea72f280
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-27 23:38:59 -0500

    dump tables?

--- interp.c
+++ interp.c
@@ -7,5 +7,21 @@
 
 
 int main(int argc, char *argv[]) {
+	char line[MAX_LINE + 1];
+
+	if (argc > 2) {
+		printf(stderr, "%s [sym-table]\n, argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if (argc ==2) {
+		build_table(argc[1]);
+	} else {
+		build_table(NULL);
+	}
+
+	printf("table");
+	dump_table();
+
 
 }
