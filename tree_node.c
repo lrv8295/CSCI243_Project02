a0e55fc8128235e202f85e8794a33098a58458cf
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-27 23:58:36 -0500

    AH

--- tree_node.c
+++ tree_node.c
@@ -4,7 +4,10 @@
 
 #include "tree_node.h"
 
-tree_node_t *make_interior(op_type_t op, char *token, tree_node_t *left, tree_node_t *right);
+tree_node_t *make_interior(op_type_t op, char *token, tree_node_t *left, tree_node_t *right) {
 
-tree_node_t *make_leaf(exp_type_t exp_type, char *token) {}
+}
 
+tree_node_t *make_leaf(exp_type_t exp_type, char *token) {
+
+}
