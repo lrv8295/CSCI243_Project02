67a1d094cd2560ba121e6bf38dc37b51fb6dd8c8
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-27 22:25:19 -0500

    make_stack

--- stack.c
+++ stack.c
@@ -4,3 +4,12 @@
 #include "stack.h"
 #include "stack_node"
 
+stack_t *make_stack(void) {
+	stack_t *stk = malloc(sizeof(stack_t));
+	if(!stk) {
+		printf(stderr, "Memory fail in make stack\n");
+		exit(EXIT_FAILURE);
+	}
+	stk->top = NULL;
+	return stk;
+}
