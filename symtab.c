2e30b670288bce919e1017b64d163436cd10bf72
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-27 23:15:38 -0500

    start

---
+++ symtab.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "symtab.h"
+
+stais symbol_t *symtab = NULL
+
+symbol_t *create_symbol(char *name, int val) {}
+
+symbol_t *lookup_table (char *variable) {}
+
+void build_table(char *filename) {}
+
+void dump_table(void){}
+
+void free_table(void) {}
